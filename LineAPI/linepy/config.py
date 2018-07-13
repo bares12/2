a6ba2f78fdfa089f2113e8db1b0058a075af209a
PH13 <31429243+PHIEZUKE@users.noreply.github.com>
2018-07-14 01:04:11 +0700

    Update config.py

--- LineAPI/linepy/config.py
+++ LineAPI/linepy/config.py
@@ -28,11 +28,11 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[304]
-    APP_VER     = '7.14.0'
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'iPhone OS'
-    SYSTEM_VER  = '10.12.0'
+    SYSTEM_NAME = 'CHROME-OS'
+    SYSTEM_VER  = '1'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
